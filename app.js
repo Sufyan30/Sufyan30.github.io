f17667bed0618355c5b1cd8df3216e6f926f08d1
aleHyer <alehyerr@gmail.com>
2022-01-13 13:43:41 +0300

    First try at hosting using GitHub.

---
+++ app.js
@@ -0,0 +1,38 @@
+alert("This website is under development.");
+
+function hover0() {
+    document.getElementById('javaisbad0').style.animation='designtxt 0.35s forwards';
+};
+function unhover0() {
+    document.getElementById('javaisbad0').style.animation='undesigntxt 0.35s forwards';
+};
+function hover1() {
+    document.getElementById('javaisbad1').style.animation='designtxt 0.35s forwards';
+};
+function unhover1() {
+    document.getElementById('javaisbad1').style.animation='undesigntxt 0.35s forwards';
+};
+function hover2() {
+    document.getElementById('javaisbad2').style.animation='designtxt 0.35s forwards';
+};
+function unhover2() {
+    document.getElementById('javaisbad2').style.animation='undesigntxt 0.35s forwards';
+};
+function hover3() {
+    document.getElementById('javaisbad3').style.animation='designtxt 0.35s forwards';
+};
+function unhover3() {
+    document.getElementById('javaisbad3').style.animation='undesigntxt 0.35s forwards';
+};
+function hover4() {
+    document.getElementById('javaisbad4').style.animation='designtxt 0.35s forwards';
+};
+function unhover4() {
+    document.getElementById('javaisbad4').style.animation='undesigntxt 0.35s forwards';
+};
+function hover5() {
+    document.getElementById('javaisbad5').style.animation='designtxt 0.35s forwards';
+};
+function unhover5() {
+    document.getElementById('javaisbad5').style.animation='undesigntxt 0.35s forwards';
+};
