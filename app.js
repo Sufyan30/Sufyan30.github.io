163a0bf14c986e8f278e1e941ded1ed542e163fc
aleHyer <alehyerr@gmail.com>
2022-02-12 14:16:48 +0300

    massive haul of stuff

--- app.js
+++ app.js
@@ -1,44 +1,9 @@
-alert("This website is under development.");
+alert("This website is under development. The mobile version is unstable.");
 
-function hover0() {
-    document.getElementById('javaisbad0').style.animation='designtxt 0.35s forwards';
+function hover(hoverid) {
+    document.getElementById(hoverid).style.animation = 'designtxt 0.35s forwards';
 };
-function unhover0() {
-    document.getElementById('javaisbad0').style.animation='undesigntxt 0.35s forwards';
-};
-function hover1() {
-    document.getElementById('javaisbad1').style.animation='designtxt 0.35s forwards';
-};
-function unhover1() {
-    document.getElementById('javaisbad1').style.animation='undesigntxt 0.35s forwards';
-};
-function hover2() {
-    document.getElementById('javaisbad2').style.animation='designtxt 0.35s forwards';
-};
-function unhover2() {
-    document.getElementById('javaisbad2').style.animation='undesigntxt 0.35s forwards';
-};
-function hover3() {
-    document.getElementById('javaisbad3').style.animation='designtxt 0.35s forwards';
-};
-function unhover3() {
-    document.getElementById('javaisbad3').style.animation='undesigntxt 0.35s forwards';
-};
-function hover4() {
-    document.getElementById('javaisbad4').style.animation='designtxt 0.35s forwards';
-};
-function unhover4() {
-    document.getElementById('javaisbad4').style.animation='undesigntxt 0.35s forwards';
-};
-function hover5() {
-    document.getElementById('javaisbad5').style.animation='designtxt 0.35s forwards';
-};
-function unhover5() {
-    document.getElementById('javaisbad5').style.animation='undesigntxt 0.35s forwards';
-};
-function hover6() {
-    document.getElementById('javaisbad6').style.animation='designtxt 0.35s forwards';
-};
-function unhover6() {
-    document.getElementById('javaisbad6').style.animation='undesigntxt 0.35s forwards';
+
+function unhover(hoverid) {
+    document.getElementById(hoverid).style.animation = 'undesigntxt 0.35s forwards';
 };
