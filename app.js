adaad010ac51d1edfbb4255f31dc2ea25d03df2f
aleHyer <alehyerr@gmail.com>
2022-07-20 16:24:20 +0300

    Big Haul

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-alert("This website is under development. The mobile version is unstable.");
-
 function hover(hoverid) {
     document.getElementById(hoverid).style.animation = 'designtxt 0.35s forwards';
 };
 
 function unhover(hoverid) {
     document.getElementById(hoverid).style.animation = 'undesigntxt 0.35s forwards';
+};
+
+function hoverimg(hoverid) {
+    document.getElementById(hoverid).style.animation = 'designtxt 0.35s forwards';
+};
+
+function unhoverimg(hoverid) {
+    document.getElementById(hoverid).style.animation = 'undesigntxt 0.35s forwards';
+};
+
+function copydc() {
+    alert("Copied!");
+    navigator.clipboard.writeText("alehyer#3767");
 };
